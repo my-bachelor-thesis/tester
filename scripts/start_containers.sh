b05a8333f1fe30e436d907f8dab37c756a9a81ae
Jan Kelemen <jan@jankelemen.net>
2022-05-17 06:05:55 +0200

    fixed small bugs

--- scripts/start_containers.sh
+++ scripts/start_containers.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-chmod +x run_solution.sh
+chmod +x scripts/run_solution.sh
 
 numberOfContainers="$1"
 ramPerContainer="$2"
@@ -8,4 +8,4 @@ dockerFile="build/package/docker-compose.yml"
 
 yq -i '.services.tester.deploy.resources.limits.memory = "'"$ramPerContainer"'M"' "$dockerFile"
 
-docker-compose -f "$dockerFile" up --scale tester="$numberOfContainers" -d
+docker compose -f "$dockerFile" up --scale tester="$numberOfContainers" -d
